d79a8abdf3d5d12a736d11b473f2feb52811b0ae
Chambers <bobhus394@gmail.com>
2022-12-06 16:01:35 +0800

    設定基本面板

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+import HeaderWrapper from './components/HeaderWrapper.vue'
 </script>
 
 <template>
+  <HeaderWrapper/>
   <RouterView />
 </template>
 
---
+++ src/components/HeaderWrapper.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class=" shadow-md ">
+    <div class="p-4 container mx-auto flex items-center justify-between">
+      <h1 class=" shrink-0">
+        <router-link class="hover:opacity-80 text-primary" to="/">PokeDex</router-link>
+      </h1>
+      <nav>
+        <ul>
+          <li v-for="item in navList" :key="item.link">
+            <router-link :to="item.link">{{ item.title }}</router-link>
+          </li>
+        </ul>
+      </nav>
+      <button class=" md:hidden" @click="(isShow = !isShow)">
+        <i v-if="isShow" class='bx bx-x align-middle bx-sm'></i>
+        <i v-else class='bx bx-menu align-middle bx-sm'></i>
+      </button>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isShow = ref(false)
+
+const navList = [
+  {
+    title: '首頁',
+    link: '/'
+  }
+]
+</script>
