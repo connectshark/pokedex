fa5207fac135a7cefba430d0002d2faba21d02e3
Chambers <bobhus394@gmail.com>
2022-12-06 15:32:08 +0800

    project init

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8080
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? '/pokedex/'
+    : '/',
+})
